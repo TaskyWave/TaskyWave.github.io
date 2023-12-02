e65814cba2c4fb576dc15f02488dd1704cb5b91c
TaskyWave <151441320+TaskyWave@users.noreply.github.com>
2023-12-02 20:36:56 +0100

    Add files via upload

---
+++ Assets/Js/BDD.js
@@ -0,0 +1,75 @@
+var SUPABASE_URL = 'https://yeetqwekspzypbgnjhvs.supabase.co'
+var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllZXRxd2Vrc3B6eXBiZ25qaHZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxMDA3NTcsImV4cCI6MjAxMTY3Njc1N30.9zzpjAwBdsoHJWQsfrHeenKASCLbtkjgpUjHJ4ggNGs'
+
+var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+window.userToken = null
+
+if (JSON.stringify(supabase.auth.user()) != null) {
+  window.location.href = "compte.html";
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  var signUpForm = document.querySelector('#sign-up')
+  signUpForm.onsubmit = signUpSubmitted.bind(signUpForm)
+
+  var logInForm = document.querySelector('#log-in')
+  logInForm.onsubmit = logInSubmitted.bind(logInForm)
+
+  var logoutButton = document.querySelector('#logout-button')
+  logoutButton.onclick = logoutSubmitted.bind(logoutButton)
+})
+
+const signUpSubmitted = (event) => {
+  event.preventDefault()
+  const email = event.target[0].value
+  const password = event.target[1].value
+
+  supabase.auth
+    .signUp({ email, password })
+    .then((response) => {
+      response.error ? alert(response.error.message) : setToken(response)
+    })
+    .catch((err) => {
+      alert(err)
+    })
+}
+
+const logInSubmitted = (event) => {
+  event.preventDefault()
+  const email = event.target[0].value
+  const password = event.target[1].value
+
+  supabase.auth
+    .signIn({ email, password })
+    .then((response) => {
+      response.error ? alert(response.error.message) : setToken(response)
+    })
+    .catch((err) => {
+      alert(err.response.text)
+    })
+}
+
+const fetchUserDetails = () => {
+  alert(JSON.stringify(supabase.auth.user()))
+}
+
+const logoutSubmitted = (event) => {
+  event.preventDefault()
+
+  supabase.auth
+    .signOut()
+    .then((_response) => {
+      alert('Logout successful')
+    })
+    .catch((err) => {
+      alert(err.response.text)
+    })
+}
+
+function setToken(response) {
+  if (response.user.confirmation_sent_at && !response?.session?.access_token) {
+    alert('Confirmation Email Sent')
+  } else {
+    alert('Logged in as ' + response.user.email)
+  }
+}
